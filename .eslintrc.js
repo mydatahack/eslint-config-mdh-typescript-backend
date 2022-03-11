c058f903bbfa214796f56e503bb8f91f28b6e9ef
mydatahack <thon6thon6@hotmail.com>
2022-03-12 10:38:47 +1100

    Add config and install dependencies
    Committer: mydatahack <thon6thon6@hotmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+const eslint = {
+  plugins: ['@typescript-eslint', 'unicorn', 'folders', 'import', 'prettier'],
+  extends: [
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+  },
+  rules: {
+    // directories
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'camelCase',
+      },
+    ],
+    'folders/match-regex': [2, '^[a-zA-Z_]+$', '/src/'],
+    'no-restricted-imports': ['error', { patterns: ['src/*'] }],
+    'import/prefer-default-export': 0,
+
+    // typescript
+    '@typescript-eslint/naming-convention': 'error',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    // other
+    'class-methods-use-this': 0,
+    'max-classes-per-file': 0,
+    'no-console': 0,
+    'prefer-destructuring': 0,
+    'no-await-in-loop': 0,
+  },
+};
+
+module.exports = eslint;
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const prettier = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+};
+
+module.exports = prettier;
